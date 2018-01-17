4d699ca75db7888063fc117d5706382abb7596bf
Andrew Friedman <andrewfriedman01@gmail.com>
2018-01-16 20:09:53 -0500

    Added App
    This is the first version of Pick Up Line Generator that GitHub has ever seen!

---
+++ Assets/Picker.js
@@ -0,0 +1,30 @@
+﻿/*  -----------------------
+	Made by Andrew Friedman
+	6/3/17 1:10 AM
+	Sh!t is fye ;)
+	-----------------------  */
+	
+#pragma strict
+import System.IO; //For Reading TXT
+
+var StartText : GameObject;
+var RandoNumbo : int;
+
+var sr : StreamReader;
+var List : TextAsset;
+var TheLine : UnityEngine.UI.Text;
+
+function Start () {
+	//List = Resources.Load("Lines", typeof(TextAsset));
+}
+
+function Generate () {
+	StartText.SetActive (false);
+	RandoNumbo = Random.Range(1,401);
+	sr = new StreamReader(new MemoryStream(List.bytes));
+	while (RandoNumbo > 0) {
+		TheLine.text = sr.ReadLine();
+		RandoNumbo--;
+	}
+	sr.Close();
+}
